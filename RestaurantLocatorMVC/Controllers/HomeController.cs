0d1cbe76efc0b5975375a1829a400d3d6af1962b
Janet Mayher <mayherjanet@gmail.com>
2019-02-19 19:06:41 -0600

    Add project files.

---
+++ RestaurantLocatorMVC/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantLocatorMVC.Models;
+
+namespace RestaurantLocatorMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Restaurant> restaurants = new List<Restaurant>();
+
+        public ActionResult Index()
+        {
+            ViewBag.restaurants = restaurants;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]        
+        public ActionResult Add(string name, string streetaddress, string city, string state, int zipcode )
+        {
+            var newrestaurant = new Restaurant(name, streetaddress, city, state, zipcode);
+            restaurants.Add(newrestaurant);
+            return Redirect("/");
+
+        }
+
+        public ActionResult Random()
+        {
+            Random randNum = new System.Random(restaurants.Count);
+
+            Restaurant objRest;
+
+            objRest = restaurants[randNum.Next(restaurants.Count)];
+
+            ViewBag.objRest = objRest;
+
+            return View();
+
+        }
+    }
+}
---
+++ RestaurantLocatorMVC/Models/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantLocatorMVC.Models
+{
+    public class Restaurant
+    {
+        public Restaurant(string name, string streetaddress, string city, string state, int zipcode)
+        {
+            Name = name;
+            StreetAddress = streetaddress;
+            City = city;
+            State = state;
+            ZipCode = zipcode;
+        }
+
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int ID { get; set; }
+    }
+}
